80f24fe926944bc9c8f23eca1103232fa3187719
Aden Earnshaw <aearnshaw@outlook.com>
2019-05-29 10:59:34 +0100

    Removing Dist contents

--- code/dist/builders/builders.module.js
+++
@@ -1,5 +0,0 @@
-define(["require", "exports", "./html-tickets-document.builder"], function (require, exports, html_tickets_document_builder_1) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    exports.HtmlTicketsDocumentBuilder = html_tickets_document_builder_1.HtmlTicketsDocumentBuilder;
-});
